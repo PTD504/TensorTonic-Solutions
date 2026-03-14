8aeef5b2c32f93fa4ef01c416de3576883ae8dec
Phan Thanh Đăng <152518457+PTD504@users.noreply.github.com>
2026-03-14 08:33:13 +0700

    Add solution for adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    """
+    Perform one AdaGrad update step.
+    """
+    w = np.array(w)
+    g = np.array(g)
+    G = np.array(G)
+
+    # Accumulate squared gradients
+    G = G + g ** 2
+
+    # Parameter Update
+    w = w - lr * g / np.sqrt(G + eps)
+
+    return (w, G)
